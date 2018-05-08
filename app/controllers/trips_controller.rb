d3db1a16192861a11f40d70bcd2ab658e844b0c4
Linda Bergen <linda.bergen@mobilejob.com>
2018-05-08 23:53:12 +0200

    migrate database from sqlite3 to postgresql

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -4,5 +4,12 @@ class TripsController < ApplicationController
   end
 
   def calculate
+    @origin = City.find_by(id: params[:trip][:origin_city])
+    @destination = City.find_by(id: params[:trip][:destination_city])
+    adelaide_cairo_express(@origin, @destination)
+  end
+
+  def adelaide_cairo_express(origin, destination)
+    origin.update_columns(time: 1, cost: 25) if (origin.name == 'Adelaide' && destination.name == 'Cairo')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   get 'trips/index'
-  get 'trips/calculate'
+  post 'trips/calculate'
 end
